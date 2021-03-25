4b72fc2aab07d6cbab6e18c0695ea042265a3800
cbrwx <81207429+cbrwx@users.noreply.github.com>
2021-03-25 02:05:17 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2785,7 +2785,7 @@ bool InitBlockIndex() {
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 50 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("0493a4435272c05fdf053d6d1ebd4f2ea200d52ee4f9a68492b2d2f20dd44863ab9634cd1a1908cd48a1c3158a76e2ba57500048d84240130d63332707fabfa8a8") << OP_CHECKSIG;
         CBlock block;
         block.vtx.push_back(txNew);
         block.hashPrevBlock = 0;
